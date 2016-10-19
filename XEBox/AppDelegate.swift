fd3b7af7798dbb89707317846f33d114daa5ecf4
Lu Yibin <robinlu@me.com>
2016-10-19 15:31:30 +0800

    Initial Commit

---
+++ XEBox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XEBox
+//
+//  Created by Lu Yibin on 19/10/2016.
+//  Copyright © 2016 Lu Yibin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XEBox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XEBox
+//
+//  Created by Lu Yibin on 19/10/2016.
+//  Copyright © 2016 Lu Yibin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
